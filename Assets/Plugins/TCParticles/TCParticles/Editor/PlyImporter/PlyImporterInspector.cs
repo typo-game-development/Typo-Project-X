2959d30d31ceead9e25822a403d9da17bcd46591
WoolMagician <francesco.podda.db@gmail.com>
2019-09-10 20:33:01 +0200

    Removed TCParticles as it was unused, moved Bezier scripts inside script folder and removed empty Plugins folder.

--- Assets/Plugins/TCParticles/TCParticles/Editor/PlyImporter/PlyImporterInspector.cs
+++
@@ -1,9 +0,0 @@
-using UnityEditor;
-using UnityEditor.Experimental.AssetImporters;
-
-namespace Pcx {
-	[CustomEditor(typeof(PlyImporter))]
-	class PlyImporterInspector : ScriptedImporterEditor {
-		protected override bool useAssetDrawPreview => false;
-	}
-}
